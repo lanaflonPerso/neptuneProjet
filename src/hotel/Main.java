0a879b9f409ff4008c55665b3a61b4cec9fccc81
MY-COMPUTER\morga <morgan.fraisse@hotmail.fr>
2018-07-06 11:25:52 +0200

    Planning de la semaine

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -9,14 +9,14 @@ import javafx.stage.Stage;
 import java.util.List;
 
 public class Main extends Application {
-    Client client = new Client( 161, "Monsieur", "toto", "tata", "1 rue des oisillions", "34000", "Montpellier", 2);
-    Client clientU = new Client(163,
+   // Client client = new Client( 161, "Monsieur", "toto", "tata", "1 rue des oisillions", "34000", "Montpellier", 2);
+    /*Client clientU = new Client(163,
                                 "madame",
                                 "tata",
                                 "titine",
                                 "2 rue des exemples",
                                 "000010", "Salopette", 3);
-    Client clientD = new Client(161,"Monsieur","titi","tata","1 rue des oisillions","34000","Montpellier", 2);
+    Client clientD = new Client(161,"Monsieur","titi","tata","1 rue des oisillions","34000","Montpellier", 2);*/
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -25,15 +25,15 @@ public class Main extends Application {
         primaryStage.show();
         MySQLDatabaseUtil.dbConnect();
         //List<Client> nbClients = ClientDAO.listclients();
-        //System.out.println(nbClients.size());
+         //System.out.println(nbClients.size());
         //PlanningDAO.listplanning();
         //List<Chambre> nbChambres= ChambreDAO.listchambres();
         /*for (Client client: ClientDAO.listclients()){
         System.out.println(client);}*/
-        /*for (Planning planning: PlanningDAO.listplanning()){
-            System.out.println(planning);}*/
-        for (Chambre chambre: ChambreDAO.listchambres()){
-            System.out.println(chambre);}
+        for (Planning planning: PlanningDAO.planningDeLaSemaine()){
+            System.out.println(planning);}
+        /*for (Chambre chambre: ChambreDAO.listchambres()){
+            System.out.println(chambre);}*/
         //ClientDAO.addClient(client);
         //ClientDAO.removeClient(clientD);
        // ClientDAO.updateClient(clientU);
--- src/hotel/PlanningDAO.java
+++ src/hotel/PlanningDAO.java
@@ -1,19 +1,23 @@
 package hotel;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
+import java.sql.Date;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+
 
 public class PlanningDAO {
 
-    public static List<Planning> listplanning (){
-        List<Planning> plannings= new ArrayList<Planning>();
+    public static List<Planning> listplanning() {
+        List<Planning> plannings = new ArrayList<Planning>();
         try {
-            ResultSet resultset=MySQLDatabaseUtil.dbExecuteQuery("SELECT * FROM planning;");
-            while(resultset.next()){
+            ResultSet resultset = MySQLDatabaseUtil.dbExecuteQuery("SELECT * FROM planning;");
+            while (resultset.next()) {
                 System.out.println(resultset.getDate("jour"));
 
                 Planning planning = new Planning();
@@ -36,4 +40,44 @@ public class PlanningDAO {
 
     }
 
+    public static List<Planning> planningDeLaSemaine() {
+
+        List<Planning> planningsS = new ArrayList<Planning>();
+
+        try {
+            LocalDate dateDebutSemaine = LocalDate.now();
+            /*LocalDate semaine = dateDuJour.minusDays(7);*/
+
+
+            //System.out.println( dateDuJour.getDayOfWeek() != DayOfWeek.FRIDAY);
+
+            while (dateDebutSemaine.getDayOfWeek() != DayOfWeek.MONDAY) {
+                dateDebutSemaine = dateDebutSemaine.minusDays(1);
+
+
+            }
+            LocalDate dateFin = dateDebutSemaine.plusDays(6);
+                ResultSet resultSemaine = MySQLDatabaseUtil.dbExecuteQuery("SELECT * FROM planning Where jour BETWEEN '" + dateDebutSemaine+ "' and '"+dateFin+"' ");
+                while (resultSemaine.next()) {
+
+                    Planning planningSemaine = new Planning();
+                    planningSemaine.setNumeroChambre(resultSemaine.getInt("chambre_numero"));
+                    planningSemaine.setJour(resultSemaine.getDate("jour").toLocalDate());
+                    planningSemaine.setReservation(resultSemaine.getInt("reservation"));
+                    planningSemaine.setPaye(resultSemaine.getInt("paye"));
+                    planningSemaine.setIdClient(resultSemaine.getInt("client_id"));
+
+                    planningsS.add(planningSemaine);
+                }
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return planningsS;
+
+    }
 }
